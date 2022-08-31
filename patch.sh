797377eb9706e0c9e1aa41206f9fc0805889a9f3
Ian Chen <ychen.desl@gmail.com>
2022-08-31 17:48:03 +0800

    Create patch.sh

---
+++ patch.sh
@@ -0,0 +1,7 @@
+# patch.sh is used to apply the NF's patch to submodule when NF's PR was created
+
+NF_LIST="nrf amf smf udr pcf udm nssf ausf upf n3iwf"
+
+for NF in ${NF_LIST}; do
+    echo "${$NF^^ + "_PATCH"}"
+done
