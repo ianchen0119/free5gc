52916479a72189e337404ab55d62b4064b4f5233
ianchen0119 <ychen.desl@gmail.com>
2022-08-31 10:05:14 +0000

    update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,5 +3,13 @@
 NF_LIST="nrf amf smf udr pcf udm nssf ausf upf n3iwf"
 
 for NF in ${NF_LIST}; do
-    echo "${$NF^^'_PATCH'}"
+    cd NFs/$NF
+    PATCH=("${NF^^}_PATCH")
+    echo ${!PATCH}
+    if [ "${!PATCH}" != "" ];
+    then
+        echo "true"
+        # curl ${!PATCH} | git apply
+    fi
+    cd ../..
 done
